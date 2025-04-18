b1185b0af5d5d4d6457aed17be564cf7f5c4be78
Arsalan Tahir <ssgemini56@gmail.com>
2025-04-18 21:30:22 +0500

    Corrected Move Logic

--- Pawn.cpp
+++ Pawn.cpp
@@ -11,57 +11,37 @@ std::vector<Position> Pawn::getValidMoves(Board* board) const {
     std::vector<Position> validMoves;
     int row = position.getRow();
     int col = position.getColumn();
-
-    int directions[4][2] = {
-        {0, 1}, {0, 2} , // forward move and double move
-        {1, 1}, {-1, 1} // capture moves (diagonal) 
-    };
     
-        int newRow1 = row + directions[0][0];
-        int newCol1 = col + directions[0][1];
-        int newRow2 = row + directions[1][0];
-        int newCol2 = col + directions[1][1];
-        int newRow3 = row + directions[2][0];
-        int newCol3 = col + directions[2][1];
-        int newRow4 = row + directions[3][0];
-        int newCol4 = col + directions[3][1];
-
-        if (newRow1 > 0 && newRow1 <= BOARD_ROWS &&
-            newCol1 > 0 && newCol1 <= BOARD_COLS) {
-            Position newPos(newRow1, newCol1);
-            Piece* pieceAtNewPos = board->getPiece(newPos);
+    int direction = (color == Color::White) ? -1 : 1;
+    
+    Position oneStep(row + direction, col);
+    if (isInBounds(oneStep) && board->getPiece(oneStep) == nullptr) {
+        validMoves.push_back(oneStep);
         
-            if (pieceAtNewPos == nullptr) {
-                validMoves.push_back(newPos);
-            } 
+        if (!hasMoved) {
+            Position twoStep(row + (2 * direction), col);
+            if (board->getPiece(twoStep) == nullptr) {
+                validMoves.push_back(twoStep);
+            }
         }
-        if (newRow2 > 0 && newRow2 <= BOARD_ROWS &&
-            newCol2 > 0 && newCol2 <= BOARD_COLS) {
-            Position newPos(newRow2, newCol2);
-            Piece* pieceAtNewPos = board->getPiece(newPos);
-
-            if (pieceAtNewPos == nullptr && !hasMoved) {
-                validMoves.push_back(newPos);
-            } 
+    }
+    
+    Position leftCapture(row + direction, col - 1);
+    Position rightCapture(row + direction, col + 1);
+    
+    if (isInBounds(leftCapture)) {
+        Piece* pieceAtLeft = board->getPiece(leftCapture);
+        if (pieceAtLeft && pieceAtLeft->getColor() != color) {
+            validMoves.push_back(leftCapture);
         }
-        if (newRow3 > 0 && newRow3 <= BOARD_ROWS &&
-            newCol3 > 0 && newCol3 <= BOARD_COLS) {
-            Position newPos(newRow3, newCol3);
-            Piece* pieceAtNewPos = board->getPiece(newPos);
-        
-            if (pieceAtNewPos != nullptr && pieceAtNewPos->getColor() != color) {
-                validMoves.push_back(newPos);
-            } 
+    }
+    
+    if (isInBounds(rightCapture)) {
+        Piece* pieceAtRight = board->getPiece(rightCapture);
+        if (pieceAtRight && pieceAtRight->getColor() != color) {
+            validMoves.push_back(rightCapture);
         }
-        if (newRow4 > 0 && newRow4 <= BOARD_ROWS &&
-            newCol4 > 0 && newCol4 <= BOARD_COLS) {
-            Position newPos(newRow4, newCol4);
-            Piece* pieceAtNewPos = board->getPiece(newPos);
-        
-            if (pieceAtNewPos != nullptr && pieceAtNewPos->getColor() != color) {
-                validMoves.push_back(newPos);
-            } 
-        }
+    }
     
     return validMoves;
 }
