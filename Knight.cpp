130a2701d4b13593cd651870d6d330f313c3aaa6
Arsalan Tahir <ssgemini56@gmail.com>
2025-04-18 21:30:08 +0500

    Corrected Move logic

--- Knight.cpp
+++ Knight.cpp
@@ -12,28 +12,26 @@ std::vector<Position> Knight::getValidMoves(Board* board) const {
     int row = position.getRow();
     int col = position.getColumn();
 
-    int directions[4][2] = {
-        {1, 2}, {-1, 2}, {-1, -2}, {1, -2}
+    int directions[8][2] = {
+        {-2, -1}, {-2, 1},  // Up 2, left/right 1
+        {2, -1},  {2, 1},   // Down 2, left/right 1
+        {-1, -2}, {1, -2},  // Left 2, up/down 1
+        {-1, 2},  {1, 2}    // Right 2, up/down 1
     };
     
     for (auto& dir : directions) {
-        int newRow = row;
-        int newCol = col;
-        newRow += dir[0];
-        newCol += dir[1];
+        int newRow = row + dir[0];
+        int newCol = col + dir[1];
 
-        if (newRow < 0 || newRow >= BOARD_ROWS || 
-            newCol < 0 || newCol >= BOARD_COLS) {
+        // Check board boundaries
+        if (!isInBounds(newRow, newCol)) {
             continue;
         }
             
         Position newPos(newRow, newCol);
         Piece* pieceAtNewPos = board->getPiece(newPos);
         
-        if (pieceAtNewPos == nullptr) {
-            validMoves.push_back(newPos);
-        } 
-        else if (pieceAtNewPos->getColor() != color) {
+        if (pieceAtNewPos == nullptr || pieceAtNewPos->getColor() != color) {
             validMoves.push_back(newPos);
         }
     }
